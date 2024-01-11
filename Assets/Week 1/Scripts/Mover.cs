22cb81c198781cdfc63f1a0a579107dc277944fa
CharlotteHicken <hickecha@sheridancollege.ca>
2024-01-11 10:38:07 -0500

    Task 2 b completed: tank firing a missile on keyboard input

--- Assets/Week 1/Scripts/Mover.cs
+++ Assets/Week 1/Scripts/Mover.cs
@@ -6,6 +6,8 @@ public class Mover : MonoBehaviour
 {
 
     float speed = 5f;
+    public GameObject prefab;
+    public Transform barrel;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +20,10 @@ public class Mover : MonoBehaviour
     {
         float keyboardInput = Input.GetAxis("Horizontal");
         transform.Translate(keyboardInput * speed * Time.deltaTime,0,0);
+
+        if( Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab, barrel.position, barrel.rotation);
+        }
     }
 }
