a45d318b11cd31b271e023e8a4f3396a47ea751e
Charlotte Hicken <charlotte.hicken@gmail.com>
2024-01-25 09:43:30 -0500

    Made missile move with .moveposition and made player have linear drag

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Missile : MonoBehaviour
 {
     public float speed = 5;
+    public Rigidbody2D rigidbody;
+    Vector2 newVector = new Vector2(3, -4);
 
     // Start is called before the first frame update
     void Start()
@@ -13,9 +15,9 @@ public class Missile : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.Translate(1 * speed * Time.deltaTime, 0, 0);
+        rigidbody.MovePosition(rigidbody.position + newVector * 1 * speed * Time.deltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
