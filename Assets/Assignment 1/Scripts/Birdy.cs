e3dfba050d33ce4ba609e2ef0c482ab56c2cf240
DESKTOP-9E3IQ19\Valued Customer <hickecha@shernet.sheridancollege.ca>
2024-01-31 14:47:53 -0500

    Started a player move script for assignment 1

---
+++ Assets/Assignment 1/Scripts/Birdy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Birdy : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 force = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+        rigidbody.AddForce(force);
+    }
+
+    void
+
+    
+}
